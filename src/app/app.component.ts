1a990466154da66e6ae6b46d32534cb7194edaab
ocampoaleja <omaira.527@hotmail.com>
2020-05-18 21:45:37 -0500

    feature: changes to styles with tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+
+import { Link } from './link/link.model'
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firstproject';
-}
+
+  titles:string;
+
+  links:Link[];
+
+  constructor(){
+    this.titles='Add a Link'
+    this.links = [
+    new Link('Angular', 'http://angular.io', 100),
+    new Link('Google' , 'http://google.com', 30)
+  ];
+  console.log(this.links)
+  }
+
+  addLink(title: HTMLInputElement, link: HTMLInputElement){
+    this.links.push(new Link(title.value,link.value));
+    return false;
+  }
+
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common'
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import {LinkComponent} from './link/link.component'
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+
+
+}
